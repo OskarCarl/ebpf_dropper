41311e66bb7a4cb326d0480d6ea1d9347ba0cc50
Oskar Carl <oskar.carl@uni-due.de>
2023-03-28 15:25:12 +0200

    cleanup, retab

--- net.h
+++ net.h
@@ -64,3 +64,15 @@ struct tcphdr {
 	__sum16	check;
 	__be16	urg_ptr;
 };
+
+// from	uapi/linux/pkt_cls.h
+#define	TC_ACT_UNSPEC       (-1)
+#define	TC_ACT_OK           0
+#define	TC_ACT_RECLASSIFY   1
+#define	TC_ACT_SHOT         2
+#define	TC_ACT_PIPE         3
+#define	TC_ACT_STOLEN       4
+#define	TC_ACT_QUEUED       5
+#define	TC_ACT_REPEAT       6
+#define	TC_ACT_REDIRECT     7
+#define	TC_ACT_TRAP         8
